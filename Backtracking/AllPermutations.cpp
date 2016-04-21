de1d2541f35d7e9b9bc7c30f81315e259ae4d8cb
debugger2017 <kartik.p.satoskar@gmail.com>
2016-04-21 16:43:51 +0530

    Backtracking

---
+++ Backtracking/AllPermutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permute(string & str,int l, int r) {
+	if(l == r) {
+		cout << str << "\n";
+	}
+	for(int i=l;i<=r;i++) {
+		swap(str[l],str[i]);
+		permute(str,l+1,r);
+		swap(str[l],str[i]);
+	}
+}
+int main(void) {
+	string str;
+	cin >> str;
+	permute(str,0,str.size()-1);
+	return 0;
+}
